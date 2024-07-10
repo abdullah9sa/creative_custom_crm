09cb0a2382635dfdde42188d48fdd102e8f8314d
Abdullah <asalih@creativeadvtech.com>
2024-07-10 10:07:29 +0300

    old validate

--- creative_custom_crm/creative_custom_crm/events.py
+++ creative_custom_crm/creative_custom_crm/events.py
@@ -2,6 +2,14 @@ import frappe
 from frappe import _, msgprint
 
 def validate_lead_status(self, args):
+    
+    self.set_full_name()
+    self.set_lead_name()
+    self.set_title()
+    self.set_status()
+    self.check_email_id_is_unique()
+    self.validate_email_id()
+    
     if (self.status not in ["Meeting and Demo", "Follow up confirmed", "Interested/Qualified", "Lead", "MQL", "SQL"]):
         frappe.throw(
             _(
